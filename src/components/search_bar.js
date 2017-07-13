ab28a1c9da134ce68bc8c4fb647f1a3424143a71
Jerker Bäckström <jerker.backstrom@outlook.com>
2017-07-13 22:16:15 +0300

    Refactor SearchBar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { searchTerm: '' };
+  }
+
   render() {
-    return <input />;
+    return (
+      <div>
+        <input
+          value={this.state.searchTerm}
+          onChange={event => this.setState({ searchTerm: event.target.value })}
+        />;
+      </div>
+    );
   }
 }
 
