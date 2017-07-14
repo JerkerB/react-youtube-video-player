c59d438d2d9f1ea87de94582cfe0cd58f3968540
Jerker Bäckström <jerker.backstrom@outlook.com>
2017-07-14 09:41:10 +0300

    Add API request for fetching youtube videos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDh3ERQbIFIZT_MydDf2_if8B-lBs3N2ao';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
