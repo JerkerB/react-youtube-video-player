0cd10ce17caa1a8d2ab4a4f1def17b956b15c547
Jerker Bäckström <jerker.backstrom@outlook.com>
2017-07-14 13:09:22 +0300

    Implement functionality for changing and playing videos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDh3ERQbIFIZT_MydDf2_if8B-lBs3N2ao';
 
@@ -9,14 +11,27 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
     YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
     });
   }
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
